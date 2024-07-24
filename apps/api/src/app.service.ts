9a45fa1479867f3bf9a6358945b0e4a96b7a2099
codebloded <rohan@jengalabs.co>
2024-07-24 07:06:02 +0530

    add nx

--- apps/api/src/app.service.ts
+++ apps/api/src/app.service.ts
@@ -1,8 +1,8 @@
 import { Injectable } from '@nestjs/common';
-
+import { add } from '@graph-n3-monorepo/sample-lib';
 @Injectable()
 export class AppService {
   getHello(): string {
-    return 'Hello Monorepo-setup!';
+    return 'Hello Monorepo-setup!' + add(1, 2);
   }
 }
--- apps/web/src/app/page.tsx
+++ apps/web/src/app/page.tsx
@@ -1,9 +1,10 @@
 import Image from "next/image";
+import { add } from "@graph-n3-monorepo/sample-lib";
 
 export default function Home() {
   return (
     <div>
-      <h1>Hello World</h1>
+      <h1>Hello World {add(6, 2)}</h1>
     </div>
   );
 }
