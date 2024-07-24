db46470783ebc31eb45302e6cd6dd50f02e637ea
codebloded <rohan@jengalabs.co>
2024-07-24 09:03:03 +0530

    husky installed

--- apps/web/src/app/layout.tsx
+++ apps/web/src/app/layout.tsx
@@ -1,21 +1,21 @@
-import type { Metadata } from "next";
-import { Inter } from "next/font/google";
+import type { Metadata } from 'next'
+import { Inter } from 'next/font/google'
 
-const inter = Inter({ subsets: ["latin"] });
+const inter = Inter({ subsets: ['latin'] })
 
 export const metadata: Metadata = {
-  title: "GraphQL N3",
-  description: "Monorepo setup with Nx, Next.js, and GraphQL",
-};
+  title: 'GraphQL N3',
+  description: 'Monorepo setup with Nx, Next.js, and GraphQL',
+}
 
 export default function RootLayout({
   children,
 }: Readonly<{
-  children: React.ReactNode;
+  children: React.ReactNode
 }>) {
   return (
     <html lang="en">
       <body className={inter.className}>{children}</body>
     </html>
-  );
+  )
 }
--- apps/web/src/app/page.tsx
+++ apps/web/src/app/page.tsx
@@ -1,10 +1,12 @@
-import Image from "next/image";
-import { add } from "@graph-n3-monorepo/sample-lib";
+import Image from 'next/image'
+import { add } from '@graph-n3-monorepo/sample-lib'
 
 export default function Home() {
+  const a = 9
+  a = 'golu'
   return (
     <div>
       <h1>Hello World {add(6, 2)}</h1>
     </div>
-  );
+  )
 }
